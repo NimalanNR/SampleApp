7aef3f228fa20f9a3f505a438457a9414d18292b
N.R <Nimalan.N.R@Honeywell.com>
2018-11-28 20:41:31 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/h282773/helloworld/AddRoom.java
@@ -0,0 +1,13 @@
+package com.example.h282773.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_room);
+    }
+}
